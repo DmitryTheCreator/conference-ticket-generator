0e81094cbafd55abbe85cf58a3189904c33bd682
DmitryTheCreator <dmitriyzamorev@gmail.com>
2025-02-13 23:22:24 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<template>
+  <div id="app">
+    <h1>Welcome to Vue + Vite + TypeScript</h1>
+  </div>
+</template>
