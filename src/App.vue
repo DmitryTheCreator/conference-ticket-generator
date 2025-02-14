eb2e892afb400094c9bb9be413d9efe23ca6fd28
DmitryTheCreator <dmitriyzamorev@gmail.com>
2025-02-14 22:29:47 +0500

    chg: Настройка линтера для FSD, скрипта для форматирования кода.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'App',
+  name: "App",
 });
 </script>
 
 <template>
   <div id="app">
     <h1>Welcome to Vue + Vite + TypeScript</h1>
+    <p>Extra spacing here</p>
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 
-
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin()
-  ],
+  plugins: [vue(), eslintPlugin()],
 });
